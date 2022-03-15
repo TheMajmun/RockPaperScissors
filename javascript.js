34f13c3d3b6460a66e350aab019f9d4bf5a4ccbc
TheJouz <thejouzenone@gmail.com>
2022-03-15 16:54:19 +0100

    created base game

--- javascript.js
+++ javascript.js
@@ -13,10 +13,62 @@ function computerPlay(){
 
 }
 
-function play(playerSelection , computerSelection) {
+function playRound(playerSelection , computerSelection) {
+    console.log(playerSelection)
+    console.log(computerSelection);
+    let player = capitalize(playerSelection);
+    if(player === computerSelection){
+        console.log("It's a tie!")
+        return(0)
+    }
+    let outcome = 0;
+    if(player === "Paper"){
+        switch (computerSelection){
+            case "Rock":
+                outcome = 1;
+                break;
+            case "Scissors":
+                outcome = 0;
+                break;
+        }
+    }
+     else if(player === "Rock"){
+        switch (computerSelection){
+            case "Scissors":
+                outcome = 1;
+                break;
+            case "Paper":
+                outcome = 0;
+                break;
+        }
+    }
+    else if(player === "Scissors"){
+            switch (computerSelection){
+                case "Paper":
+                    outcome = 1;
+                    break;
+                case "Rock":
+                    outcome = 0;
+                    break;
+            }
+    }
 
-    let player = playerSelection.toLowerCase();
-    player = player.charAt(0).toUpperCase();
+    switch(outcome) {
+
+        case 0:
+            console.log("toobad you lost")
+            return(-1)
+            break;
+        case 1:
+            console.log("you won yay")
+            return(1)
+            break;
+
+
+
+    }
+
+
 
 
 
@@ -24,6 +76,37 @@ function play(playerSelection , computerSelection) {
 
 }
 
-// paper najmanji
-// rock srednji
-// sci najveci
+
+
+function game() {
+    let score = 0
+    for (let i = 0; i < 5; i++) {
+        let x = playRound(prompt("Please enter Rock Paper Or Scissors"), computerPlay())
+        score = score + x;
+    }
+    if(score === 0){
+        console.log('its a tie')
+    }
+    if(score > 0){
+        console.log('you won')
+    }
+    if(score < 0){
+        console.log('you lost')
+    }
+    
+}
+
+
+
+
+
+
+
+    
+function capitalize(string){
+    let initial = string;
+    let secondPart = initial.slice(1);
+    secondPart = secondPart.toLowerCase();
+    let capital = initial.charAt(0).toUpperCase();
+    return(capital+secondPart)
+}
